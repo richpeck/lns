4485381d9805a3f018fb057ab7447a10bf803e74
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-12-23 21:31:58 +0000

    @customer test

--- app/app.rb
+++ app/app.rb
@@ -166,6 +166,24 @@ class App < Sinatra::Base
     # => Ensures we're able to only accept inbound requests with certain parameters
     required_params :customer_id if request.accept.map{ |item| item.to_s }.include?("application/json") # => Only if JSON request
 
+    # => POST = the user has sent data to the service
+    # => Allows us to change/manage the @customer object
+    @customer = Customer.create_with({
+        customer_name:            params.try(:[], :customer_name),
+        gender:                   params.try(:[], :gender),
+        height:                   params.try(:[], :height),
+        weight:                   params.try(:[], :weight),
+        neck:                     params.try(:[], :neck),
+        shoulder_width:           params.try(:[], :shoulder_width),
+        sleeve_length:            params.try(:[], :sleeve_length),
+        bicep_circumference:      params.try(:[], :bicep_circumference),
+        wrist_circumference:      params.try(:[], :wrist_circumference),
+        chest_bust_circumference: params.try(:[], :chest_bust_circumference),
+        waist_circumference:      params.try(:[], :waist_circumference),
+        lower_waist:              params.try(:[], :lower_waist),
+        hips_seat:                params.try(:[], :hips_seat)
+    }).find_or_create_by({id: params[:customer_id]}) # => Doesn't cause error if not found (https://stackoverflow.com/a/9604617/1143732)
+
     # => POST
     # => Allows us to perform things with data
     if request.post?
