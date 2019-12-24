a42959d89b333ead07db3f1c664e5bfa18137ffa
Richard Peck <rpeck@frontlineutilities.co.uk>
2019-12-24 13:27:34 +0000

    SecurityUtils

--- app/app.rb
+++ app/app.rb
@@ -205,7 +205,7 @@ class App < Sinatra::Base
     data = request.body.read
     verified = verify_webhook(data, env["HTTP_X_SHOPIFY_HMAC_SHA256"])
 
-    puts verified.inspect()
+    puts params
 
     # => Only needs to capture customer_id (params[:id])
     # => We store this because we can
